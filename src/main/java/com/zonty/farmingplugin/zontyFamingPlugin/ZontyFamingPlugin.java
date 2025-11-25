880ccd2b5c2e2dbeb961803d393a882324a99d4c
Zonty228 <r.shvaybovich@edu.centraluniversity.ru>
2025-11-25 09:03:41 +0300

    A fresh project setup with minecraft dev plugin on paper

---
+++ src/main/java/com/zonty/farmingplugin/zontyFamingPlugin/ZontyFamingPlugin.java
@@ -0,0 +1,17 @@
+package com.zonty.farmingplugin.zontyFamingPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZontyFamingPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
